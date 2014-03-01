059815af4d44bd27ef30b6efb0ee8e469a98460e
Benyoung Wong <ivane1984@gmail.com>
2014-03-01 23:07:40 +0800

    Display Instagram Photo
    Display instagram photo on timeline and single page.

--- itwitter.js
+++ itwitter.js
@@ -40,10 +40,31 @@ window.onload = function() {
     }
     setInterval(ListenQuoteTweet,500);
 
+    //Instagram Photo | Timeline
+    function InstagramLink() {
+        $('.stream-item').click(function() {
+            var Instagram_Link = $(this).find('.js-display-url').text();
+            if (Instagram_Link.match(/instagram\.com\/p\/\w{10}\//) && $(this).find('.cards-instagram').length == 0) {
+                var Instagram_Link_photo = "http://"+Instagram_Link+"media/?size=l";
+                $(this).find('.stream-item-footer').after('<div data-card-url="'+Instagram_Link_photo+'" data-card-type="photo" class="cards-base cards-multimedia cards-instagram" data-element-context="platform_photo_card"><div class="cards-media-container js-media-container"><a href="'+Instagram_Link_photo+'" class="media media-thumbnail twitter-timeline-link media-forward is-preview" data-url="'+Instagram_Link_photo+'" data-resolved-url-large="'+Instagram_Link_photo+'"><div class="is-preview"><img src="'+Instagram_Link_photo+'" width="100%" alt="Embedded image permalink" /></div></a></div></div>');
+            }
+        });
+    }
+    setInterval(InstagramLink,500);
+
+    //Instagram Photo | Single Permalink
+    $(document).ready(function () {
+        var Instagram_Link = $('.wrapper-permalink .tweet-text').find('.js-display-url').eq(0).text();
+        if (Instagram_Link.match(/instagram\.com\/p\/\w{10}\//) && $('.wrapper-permalink .tweet-text').eq(0).find('.cards-instagram').length == 0) {
+            var Instagram_Link_photo = "http://"+Instagram_Link+"media/?size=l";
+            $('.wrapper-permalink .permalink-tweet').eq(0).find('.stream-item-footer').after('<div data-card-url="'+Instagram_Link_photo+'" data-card-type="photo" class="cards-base cards-multimedia cards-instagram" data-element-context="platform_photo_card"><div class="cards-media-container js-media-container"><a href="'+Instagram_Link_photo+'" class="media media-thumbnail twitter-timeline-link media-forward is-preview" data-url="'+Instagram_Link_photo+'" data-resolved-url-large="'+Instagram_Link_photo+'"><div class="is-preview"><img src="'+Instagram_Link_photo+'" width="100%" alt="Embedded image permalink" /></div></a></div></div>');
+        }
+    });
+
     //Quote Tweet
 //    var ulLength = 0;
 //    setInterval(function(){
-//        if ($('li.stream-item').length > ulLength) {
+//        if ($('li.stream-item').length > ulLensgth) {
 //            if($(this).find('li').hasClass('action-quote-tweet')) {} else {
 //                $(this).find('li.action-reply-container').after('<li class="action-quote-tweet"><a class="with-icn js-tooltip" id="QuoteTweet" href="#"><span class="Icon Icon--retweet"></span> <b>Quote Tweet</b></a></li>');
 //            }
